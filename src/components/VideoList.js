89790c96c60ed33fc936e76ef9f4f229aac9aa92
anharvey05 <amy.harvey@onshoreoutsourcing.com>
2019-10-04 08:31:23 -0500

    Committing changes through lesson 124

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     //Loops through each video, returns a list of VideoItem objects
     //Saved to renderedList
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />;
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div>
