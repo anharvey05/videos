49c3495c9482419b45c09bc40ee1ef226e4a1e26
anharvey05 <amy.harvey@onshoreoutsourcing.com>
2019-10-04 10:29:11 -0500

    Through lesson 128

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList = ({ videos, onVideoSelect }) => {
     //Loops through each video, returns a list of VideoItem objects
     //Saved to renderedList
     const renderedList = videos.map((video) => {
-        return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div>
