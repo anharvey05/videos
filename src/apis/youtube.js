1592ac52eb12d60a1b9c5263c7344bbb9d0257ba
anharvey05 <55111161+anharvey05@users.noreply.github.com>
2019-10-22 07:49:01 -0500

    Updated api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBApvNU1EIg7kF4lR-ML7G7YL_IxadJIyo';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
